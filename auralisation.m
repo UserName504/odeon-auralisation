eb387f35bcf476eda280a3ab86cf8e49f92c3bee
UserName504 <dszalai@hotmail.co.uk>
2022-04-21 15:58:51 +0100

    More of the same.

--- auralisation.m
+++ auralisation.m
@@ -1,14 +1,14 @@
-function MidsidefromBformatAuralisation(filenameSource, filenameIR, outputname)
+function Auralisation(filenameSource, filenameIR, outputname)
 
 [s fs2]  = audioread(filenameSource); % Load source audio here.
 [IR fs1] = audioread(filenameIR);     % Load impulse response here.
 
-if fs1 == fs2 % Ensure that the sample rates align.
+if fs1 == fs2 % Make sure that the sample rates align.
 
 MS_IR(:, 1) = IR(:,1) + 0.707*IR(:,3);
 MS_IR(:, 2) = IR(:,1) - 0.707*IR(:,3);
 N   = length(IR) + length(s);
-out = ifft( (fft(s(:,1),N)).*fft(MS_IR, N));
+out = ifft((fft(s(:,1),N)).*fft(MS_IR, N));
 out = out./max(abs(out(:))) * 0.99;
 audiowrite(outputname, out, fs1,'BitsPerSample', 24);
 else
